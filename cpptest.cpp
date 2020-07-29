3f4d3052bda7e996b1672a0ef1c40e26bbfe587d
gift-zg <651408230@qq.com>
2020-07-29 15:44:58 +0800

    第一次通过git提交c++源文件

---
+++ cpptest.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Please input two numbers:"<<endl;
+    int value1,value2
+    cin>>value1>>value2;
+    cout<<value1+value2<<endl;
+    return 0;
+}
+    
+
+
